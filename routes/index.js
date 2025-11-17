80fa56bea765e82026a6df3c0f289578bf464c22
averamoleres001-boop <averamoleres001@converse.edu>
2025-11-17 11:30:36 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page - serve hw5-work.html form */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { showTable: false, formData: {} });
+});
+
+/* POST home page */
+router.post('/', function(req, res, next) {
+  res.render('index', { showTable: true, formData: req.body });
 });
 
 module.exports = router;
